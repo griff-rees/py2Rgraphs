2ee195ac73b6ae773f20c217f23824a67dd241b7
Layik Hama <layik.hama@gmail.com>
2021-05-20 23:21:07 +0100

    crude python virtualenv setup for .onLoad

--- R/install-python-dependencies.R
+++ R/install-python-dependencies.R
@@ -3,39 +3,17 @@
 #' This provides a namespace placeholder for loading the python networkx library
 networkx <- NULL
 
-#' Install the python networkx library
-#'
-#' This is an option for installing networkx if it isn't added automatically
-#' @param method string passed to reticulate::py_install 'method' parameter
-#' @param conda string passed to reticulate::py_install 'conda' parameter
-install_networkx <- function(method = "auto", conda = "auto") {
-  reticulate::py_install("networkx", method = method, conda = conda)
-}
-
-#' Placeholder for the python pandas package
-#'
-#' This provides a namespace placeholder for loading the python pandas library
-pandas <- NULL
-
-#' Install the python pandas library
-#'
-#' This is an option for installing pandas if it isn't added automatically
-#' @param method string passed to reticulate::py_install 'method' parameter
-#' @param conda string passed to reticulate::py_install 'conda' parameter
-install_pandas <- function(method = "auto", conda = "auto") {
-  reticulate::py_install("pandas", method = method, conda = conda)
-}
-
-
 #' Install all needed python dependencies
 #'
 #' Install python dependencies not currently in the loaded in the current
 #' reticulate python environment
-install_dependencies <- function() {
+#' @param method string passed to reticulate::py_install 'method' parameter
+#' @param conda string passed to reticulate::py_install 'conda' parameter
+install_dependencies <- function(method = "auto", conda = "auto") {
   if (!reticulate::py_module_available("networkx")) {
-    install_networkx()
+    reticulate::py_install("networkx", method = method, conda = conda)
   }
   if (!reticulate::py_module_available("pandas")) {
-    install_pandas()
+    reticulate::py_install("pandas", method = method, conda = conda)
   }
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,17 @@
 .onLoad <- function(libname, pkgname) {
   reticulate::configure_environment(pkgname, force = TRUE)
+  if(length(reticulate::virtualenv_list()) == 0) {
+    # TODO ask the user
+    message("No Python environment found. Creating one at root: ", 
+            reticulate::virtualenv_root())
+    v <- reticulate::virtualenv_create()
+    # DO NOT feel good about this even with the message
+    message("Setting Python environment to: ", v)
+    Sys.setenv(RETICULATE_PYTHON_ENV=v)
+  } else {
+    
+  }
+  # TODO again we should ask the user
   install_dependencies()
   networkx <<- reticulate::import("networkx", delay_load = TRUE)
   pandas <<- reticulate::import("pandas", delay_load = TRUE)
